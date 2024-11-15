834f685e2dfc9e0e28a4a278306030321c85253e
Fabian Ferno <fabianferno@gmail.com>
2024-11-15 09:13:51 +0700

    Add transcripts webhook

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { MongoClient, ObjectId } from "mongodb";
 import { Transcript } from "../types";
 
 const app: Express = express();
-const port = process.env.PORT || 5000;
+const port = parseInt(process.env.PORT || "5000");
 
 // MongoDB Connection
 const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost:27017";
@@ -151,7 +151,7 @@ app.use(
 
 // Start server
 connectToDb().then(() => {
-  app.listen(port, () => {
+  app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
   });
 });
